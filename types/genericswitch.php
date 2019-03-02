42dd31ddf5318dcc39d8e2d6a6319862919b81a7
stigkristiansen <stigkristiansen@outlook.com>
2019-03-02 12:00:05 +0100

    .

---
+++ types/genericswitch.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+class DeviceTypeGenericSwitch {
+    private static $implementedType = 'SWITCH';
+    private static $implementedTraits = [
+        'OnOff'
+    ];
+    
+	private static $displayStatusPrefix = false;
+    
+	//use HelperDeviceType;
+    
+	public static function getPosition(){
+        return 50;
+    }
+    
+	public static function getCaption(){
+        return 'Generic Switch';
+    }
+    
+	public static function getTranslations(){
+        return [
+            'no' => [
+                'Generic Switch' => 'Generisk bryter',
+                'Variable'       => 'Variabel'
+            ]
+        ];
+    }
+}
+
+DeviceTypeRegistry::register('GenericSwitch');
+
+?>
